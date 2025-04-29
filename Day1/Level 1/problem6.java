d1b00eae40557f89cabe887a23aa4b80e030c0e2
Kaushik0013 <122600917+Kaushik0013@users.noreply.github.com>
2025-04-29 12:22:44 +0530

    Delete Day1 directory

--- Day1/Level 1/problem6.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        int fees = 125000;
-        int discountPercent = 10;
-        double discount = (fees / discountPercent);
-        double finalFee = fees - discount;
-        System.out.println("The discount amount is INR :" + discount + "and final discounted fee is INR" + finalFee);
-    }
-}
