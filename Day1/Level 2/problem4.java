8797860c984a7cae02df9d2ed52fb45b26516021
Kaushik0013 <kaushikramesh303@gmail.com>
2025-04-29 12:42:08 +0530

    Removed Day1 and main from day-4 branch

--- Day1/Level 2/problem4.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double Celsius = sc.nextDouble();
-        double farenheitResult = ((Celsius) * (9 / 5)) + 32;
-        System.out.println("The"+Celsius+" celsius is"+ farenheitResult+" fahrenheit");    }
-}
--- Day1/Level 2/problem5.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int fahrenheit = sc.nextInt();
-        int celsiusResult = (fahrenheit - 32) * (5 / 9);
-        System.out.println(" The" + fahrenheit + "fahrenheit is" + celsiusResult + "celsius);
-        }
-    }
