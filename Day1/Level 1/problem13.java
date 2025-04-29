0dcb3b37a18885e6cceb67b1580e8a0111fba73a
Kaushik0013 <122600917+Kaushik0013@users.noreply.github.com>
2025-04-29 12:35:43 +0530

    Delete Day1 directory

--- Day1/Level 1/problem13.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int side = sc.nextInt();
-        int perimeter = side * 4;
-        System.out.println("The length of the side is" + side + "whose perimeter is" + perimeter);
-    }
-}
